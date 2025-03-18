d964a00a9daec97b2d2fbd55c09466fbe719a4f0
Denis Pyatirov <denis.pyatirov@gmail.com>
2025-03-18 23:22:48 +0300

    Some moves with dbs

--- CompModeling/AddReactionWindow.xaml.cs
+++ CompModeling/AddReactionWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace CompModeling
     public partial class AddReactionWindow : Window
     {
         public event Action? ReactionAdded;
+        public ObservableCollection<Reaction>? allReactions { get; private set; }   
         public AddReactionWindow()
         {
             InitializeComponent();
@@ -30,37 +32,64 @@ namespace CompModeling
 
         private async void BtnSave_Click(object sender, RoutedEventArgs e)
         {
-            var newReaction = new Reaction
-            {
-                ID = 11,
-                Inp1 = txtInp1.Text,
-                Inp2 = txtInp2.Text,
-                Inp3 = txtInp3.Text,
-                Prod = txtProd.Text,
-                KInp1 = int.Parse(txtKInp1.Text),
-                KInp2 = int.Parse(txtKInp2.Text),
-                KInp3 = int.Parse(txtKInp3.Text),
-                KProd = int.Parse(txtKProd.Text),
-                Ind1 = int.Parse(txtInd1.Text),
-                Ind2 = int.Parse(txtInd2.Text),
-                Ind3 = int.Parse(txtInd3.Text)
-            };
-
             using (var context = new ApplicationContext())
             {
+                await using var transaction = await context.Database.BeginTransactionAsync();
+
+                // Добавляем компоненты в BaseForms
+                var inp1 = await GetOrCreateBaseForm(context, txtInp1.Text);
+                var inp2 = await GetOrCreateBaseForm(context, txtInp2.Text);
+                var inp3 = await GetOrCreateBaseForm(context, txtInp3.Text);
+
+                // Добавляем продукт в FormingForms
+                var product = await GetOrCreateFormingForm(context, txtProd.Text);
+
+                // Создаем реакцию
+                var newReaction = new Reaction
+                {
+                    Inp1 = inp1.Name,
+                    Inp2 = inp2.Name,
+                    Inp3 = inp3.Name,
+                    Prod = product.Name,
+                    KInp1 = int.Parse(txtKInp1.Text),
+                    KInp2 = int.Parse(txtKInp2.Text),
+                    KInp3 = int.Parse(txtKInp3.Text),
+                    KProd = int.Parse(txtKProd.Text),
+                    Ind1 = int.Parse(txtInd1.Text),
+                    Ind2 = int.Parse(txtInd2.Text),
+                    Ind3 = int.Parse(txtInd3.Text)
+                };
+
                 context.Reactions.Add(newReaction);
                 await context.SaveChangesAsync();
-                var exists = await context.Reactions.AnyAsync(r => r.ID == newReaction.ID);
-                if (!exists)
-                {
-                    MessageBox.Show($"Реакция с ID {newReaction.ID} не найдена!");
-                    return;
-                }
-            }
+                await transaction.CommitAsync();
 
-            ReactionAdded?.Invoke();
-            Close();
+                MessageBox.Show("Реакция успешно сохранена!");
+            }
         }
 
+        private async Task<BaseForm> GetOrCreateBaseForm(ApplicationContext context, string name)
+        {
+            var form = context.BaseForms.FirstOrDefault(f => f.Name == name);
+            if (form == null)
+            {
+                form = new BaseForm { Name = name };
+                context.BaseForms.Add(form);
+                await context.SaveChangesAsync();
+            }
+            return form;
+        }
+
+        private async Task<FormingForm> GetOrCreateFormingForm(ApplicationContext context, string name)
+        {
+            var form = context.FormingForms.FirstOrDefault(f => f.Name == name);
+            if (form == null)
+            {
+                form = new FormingForm { Name = name };
+                context.FormingForms.Add(form);
+                await context.SaveChangesAsync();
+            }
+            return form;
+        }
     }
 }
--- CompModeling/obj/Debug/net8.0-windows/App.g.cs
+++ CompModeling/obj/Debug/net8.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace CompModeling {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace CompModeling {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
         public static void Main() {
             CompModeling.App app = new CompModeling.App();
             app.InitializeComponent();
